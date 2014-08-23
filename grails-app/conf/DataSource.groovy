fcdae0789e9c35799c96cf9ebe4db35fabb791d3
AJ DaNelz <andrew.danelz@m33integrated.com>
2014-08-23 14:30:20 -0400

    according to prophecy, add database

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,11 @@
+import org.hibernate.dialect.PostgreSQLDialect
+
 dataSource {
     pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "org.postgresql.Driver"
+    dialect = PostgreSQLDialect
+    username = "postgres"
+    password = "postgres"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -18,7 +20,7 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:postgresql://localhost:5433/food_run"
         }
     }
     test {
